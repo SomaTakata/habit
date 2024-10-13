68748fc06882d3f2789e5230be39a8e02d72884f
SomaTakata <somatakata@gmail.com>
2024-10-13 13:53:09 +0900

    不必要ファイルの消去

--- webapp/src/components/layout/Container.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from "react";
-
-export function Container({ children }: { children: ReactNode }) {
-  return <div className="p-4">{children}</div>;
-}
--- webapp/src/components/layout/Header.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from "react";
-
-export function Header({ children }: { children: ReactNode }) {
-  return <h2 className="sticky top-0 z-20 p-4 bg-white shadow">{children}</h2>;
-}
