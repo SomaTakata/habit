d62eaaaad1df3b3d5b80f0000fd1baeb616cb050
SomaTakata <somatakata@gmail.com>
2024-10-14 11:38:16 +0900

    GoalCardの縦幅を修正

--- webapp/src/features/Goal/GoalCard.tsx
+++ webapp/src/features/Goal/GoalCard.tsx
@@ -17,7 +17,7 @@ const GoalCard: React.FC<GoalCardProps> = ({ color, goalText, count }) => {
 
   return (
     <div
-      className={`flex flex-col items-stretch p-2.5 pb-3 gap-3 ${color} rounded-lg max-w-full`}
+      className={`flex flex-col items-stretch p-2.5 pb-4 gap-3 ${color} rounded-lg max-w-full`}
     >
       <GoalCalendar
         modifiers={{
@@ -29,7 +29,7 @@ const GoalCard: React.FC<GoalCardProps> = ({ color, goalText, count }) => {
       />
       <div className="flex flex-col gap-1 text-primary-foreground">
         <div className="flex gap-1 justify-center">
-          <Flame size={16} />
+          <Flame size={16} fill="white" />
           <p className="text-sm font-semibold">{count}</p>
         </div>
         <p className="text-base tracking-wider font-bold text-center">
--- webapp/src/features/Goal/GoalPage.tsx
+++ webapp/src/features/Goal/GoalPage.tsx
@@ -6,16 +6,13 @@ import GoalCard from "./GoalCard";
 
 const GoalPage = () => {
   return (
-    <div className="grid grid-cols-2 gap-2 justify-between p-3 pb-4">
-      {/* GoalCard 1 */}
+    <div className="grid grid-cols-2 gap-2 justify-between p-3 pb-24">
       <GoalCard color="bg-blue-400" goalText="朝ご飯を食べる" count={12} />
 
-      {/* GoalCard 2 */}
       <GoalCard color="bg-orange-400" goalText="昼ご飯を食べる" count={8} />
 
-      {/* プラスボタン (新しいGoalCardを追加) */}
-      <div className="border-2 w-full h-[216px] rounded-md flex items-center justify-center">
-        <Plus size={40} className="text-muted-foreground/70" />
+      <div className="border-2 border-border w-full h-[218px] rounded-md flex items-center justify-center">
+        <Plus size={40} className="text-muted-foreground/50" />
       </div>
     </div>
   );
