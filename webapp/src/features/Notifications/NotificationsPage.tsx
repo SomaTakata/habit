40ae01927e67c9e0e8450df3b15a2dd807392fd4
SomaTakata <somatakata@gmail.com>
2024-10-17 15:48:20 +0900

    NotificationCardコンポーネントの作成

--- webapp/src/features/Notifications/NotificationsPage.tsx
+++ webapp/src/features/Notifications/NotificationsPage.tsx
@@ -1,17 +1,15 @@
-import Link from "next/link";
+import React from "react";
+import NotificationCard from "./NotificationCard";
 import {
   NotificationTabs,
   NotificationTabsContent,
   NotificationTabsList,
   NotificationTabsTrigger,
 } from "./NotificationTabs";
-import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
 
 const NotificationsPage = () => {
   return (
-    <div className="flex flex-col p-3 px-5  pb-[78px]">
+    <div className="flex flex-col p-3 px-5 pb-[78px]">
       <NotificationTabs
         defaultValue="all"
         className="flex flex-col items-center "
@@ -25,106 +23,42 @@ const NotificationsPage = () => {
         </NotificationTabsList>
         <NotificationTabsContent className="pt-10" value="all">
           <div className="flex flex-col gap-7">
-            <div className="flex w-full gap-2 items-center">
-              <div className="flex gap-3 items-center">
-                <Link href="/my-page">
-                  <Avatar className="w-[40px] h-[40px]">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </Link>
-                <p className="text-sm tracking-wide font-medium">
-                  <span className="font-semibold">soma_takata</span>
-                  があなたの投稿にいいねしました。
-                  <span className="text-muted-foreground/50 ml-1">2時間前</span>
-                </p>
-              </div>
-              <div className="h-10 w-10 relative aspect-square">
-                <Image
-                  src="/breakfast.jpeg"
-                  fill
-                  alt="Image"
-                  className="object-cover rounded-md"
-                />
-              </div>
-            </div>
-            <div className="flex w-full gap-2 items-center">
-              <div className="flex gap-3 items-center">
-                <Link href="/my-page">
-                  <Avatar className="w-[40px] h-[40px]">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </Link>
-                <p className="text-sm tracking-wide font-medium">
-                  <span className="font-semibold">soma_takata</span>
-                  があなたの投稿にコメントしました。
-                  <span className="text-muted-foreground/50 ml-1">2時間前</span>
-                </p>
-              </div>
-              <div className="h-10 w-10 relative aspect-square">
-                <Image
-                  src="/breakfast.jpeg"
-                  fill
-                  alt="Image"
-                  className="object-cover rounded-md"
-                />
-              </div>
-            </div>
+            <NotificationCard
+              username="soma_takata"
+              actionType="like"
+              time="2時間前"
+              avatarSrc="https://github.com/shadcn.png"
+              imageSrc="/breakfast.jpeg"
+            />
+            <NotificationCard
+              username="soma_takata"
+              actionType="comment"
+              time="2時間前"
+              avatarSrc="https://github.com/shadcn.png"
+              imageSrc="/breakfast.jpeg"
+            />
           </div>
         </NotificationTabsContent>
         <NotificationTabsContent className="pt-10" value="comment">
           <div className="flex flex-col gap-7">
-            <div className="flex w-full gap-2 items-center">
-              <div className="flex gap-3 items-center">
-                <Link href="/my-page">
-                  <Avatar className="w-[40px] h-[40px]">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </Link>
-                <p className="text-sm tracking-wide font-medium">
-                  <span className="font-semibold">soma_takata</span>
-                  があなたの投稿にコメントしました。
-                  <span className="text-muted-foreground/50 ml-1">2時間前</span>
-                </p>
-              </div>
-              <div className="h-10 w-10 relative aspect-square">
-                <Image
-                  src="/breakfast.jpeg"
-                  fill
-                  alt="Image"
-                  className="object-cover rounded-md"
-                />
-              </div>
-            </div>
+            <NotificationCard
+              username="soma_takata"
+              actionType="comment"
+              time="2時間前"
+              avatarSrc="https://github.com/shadcn.png"
+              imageSrc="/breakfast.jpeg"
+            />
           </div>
         </NotificationTabsContent>
         <NotificationTabsContent className="pt-10" value="like">
           <div className="flex flex-col gap-7">
-            <div className="flex w-full gap-2 items-center">
-              <div className="flex gap-3 items-center">
-                <Link href="/my-page">
-                  <Avatar className="w-[40px] h-[40px]">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </Link>
-                <p className="text-sm tracking-wide font-medium">
-                  <span className="font-semibold">soma_takata</span>
-                  があなたの投稿にいいねしました。
-                  <span className="text-muted-foreground/50 ml-1">2時間前</span>
-                </p>
-              </div>
-              <div className="h-10 w-10 relative aspect-square">
-                <Image
-                  src="/breakfast.jpeg"
-                  fill
-                  alt="Image"
-                  className="object-cover rounded-md"
-                />
-              </div>
-            </div>
+            <NotificationCard
+              username="soma_takata"
+              actionType="like"
+              time="2時間前"
+              avatarSrc="https://github.com/shadcn.png"
+              imageSrc="/breakfast.jpeg"
+            />
           </div>
         </NotificationTabsContent>
       </NotificationTabs>
