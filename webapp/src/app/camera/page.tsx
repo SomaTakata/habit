30c1e3337c3d4df7ab5863acd4ab7720e8309e00
SomaTakata <somatakata@gmail.com>
2024-10-21 14:01:17 +0900

    CameraPageの作成

---
+++ webapp/src/app/camera/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CameraPage from "@/features/Camera/CameraPage";
+
+const page = () => {
+  return <CameraPage />;
+};
+
+export default page;
---
+++ webapp/src/features/Camera/CameraPage.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as React from "react";
+
+const CameraPage = () => {
+  return <div className="flex flex-col gap-10 p-5 pb-24">CameraPage</div>;
+};
+
+export default CameraPage;
